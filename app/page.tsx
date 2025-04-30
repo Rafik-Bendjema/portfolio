a6594df69c353f615b95b123dac5ee8198e92c2d
Rafik-Bendjema <bendjema.rafik3@gmail.com>
2025-04-30 12:29:15 +0100

    image optimization , reltive path added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MainBody from './mainBody';
 import Projects from './projects';
 import WhoAmI from './whoAmI';
 import Copyright from './copyright';
-import Skills from '@/app/components/Skills';
+import Skills from './components/Skills';
 
 export default function Home() {
   return (
